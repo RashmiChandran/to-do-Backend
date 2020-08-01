5c1002832a6fd468749812e6c82a486002f4c922
RashmiChandran <rashmichandran3@gmail.com>
2020-08-01 20:21:12 +0530

    Added access control origin

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.listen(PORT, function () {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
